3611872b4ecc145c1c184e660d19e8ab07f7d449
Radu Maier <ariademur@webserv.co>
2023-08-27 10:33:34 +0200

    Add CommonView.

---
+++ src/WebServCo/View/CommonView.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\View;
+
+use WebServCo\View\Contract\ViewInterface;
+
+/**
+ * A common View that could be used by other views.
+ */
+final class CommonView extends AbstractView implements ViewInterface
+{
+    public function __construct(
+        // idea: set this dynamically as a route attribute by using a middleware.
+        public readonly string $baseUrl,
+        public readonly string $currentUrl,
+    ) {
+    }
+}
