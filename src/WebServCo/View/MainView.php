8aa12450de4cfc53da413252bdf2099c4e78a380
Radu Maier <ariademur@webserv.co>
2023-08-27 10:53:37 +0200

    MainView

---
+++ src/WebServCo/View/MainView.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\View;
+
+use WebServCo\View\Contract\ViewInterface;
+
+/**
+ * An example main view that could be used by applications.
+ */
+final class MainView extends AbstractView implements ViewInterface
+{
+    public function __construct(public readonly CommonView $commonView, public readonly string $data)
+    {
+    }
+}
